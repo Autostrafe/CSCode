0e89a8e69c645b8ec2f974c6c3d1b201657a47f2
Autostrafe <Levibautista360@gmail.com>
2024-02-29 11:51:05 -0600

    Sketch v1.0
    Added Circle

---
+++ A1 Character/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+}
+
+function draw() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  circle(windowWidth/2, windowHeight/2, 100)
+}

01d773489375e2a5417169637ec90443c1e28a40
Autostrafe <Levibautista360@gmail.com>
2024-03-05 12:09:24 -0600

    Created Sword
    Created Sword

--- A1 Character/sketch.js
+++ A1 Character/sketch.js
@@ -9,8 +9,28 @@
 function setup() {
 }
 
+// function draw() {
+//   createCanvas(windowWidth, windowHeight,WEBGL);
+//   background(50);
+//   fill(170)
+
+//   rect(width/2-100, height/2-100, 200, 200,0,75)
+
+// }
+
 function draw() {
-  createCanvas(windowWidth, windowHeight);
-  background(220);
-  circle(windowWidth/2, windowHeight/2, 100)
+  createCanvas(windowWidth, windowHeight,WEBGL);
+  background(0);
+
+  fill(240, 150, 150);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(20, 6, 45);
+  translate(0,80)
+  cylinder(10,150,2);
+  translate(0,100)
+  cone(10,50,1)
+  translate(0,-210)
+  cylinder(5,50,8);
+
 }
